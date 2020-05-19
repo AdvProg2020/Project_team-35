36654ee0e4af8840629dd7dbb461f9094223dda5
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-05-19 05:13:14 +0430

    change manager methods to return boolean

---
+++ src/test/java/Controller/ManagerBossTest.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class ManagerBossTest {
+
+    @Test
+    public void sortDiscountCodesWithField() {
+    }
+}
