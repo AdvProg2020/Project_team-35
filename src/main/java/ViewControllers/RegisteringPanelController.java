6fea3ded8177243070cb53caccdfa85503d1931a
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2020-06-13 13:30:08 +0430

    login process phase 2

--- src/main/java/ViewControllers/RegisteringPanelController.java
+++ src/main/java/ViewControllers/RegisteringPanelController.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 
 public class RegisteringPanelController {
 
-    public void startLogin(MouseEvent mouseEvent) {
+    public void startLogin(MouseEvent mouseEvent) throws IOException {
+        Main.setRoot("LoginPage", "login");
     }
 
     public void startRegister(MouseEvent mouseEvent) throws IOException {
