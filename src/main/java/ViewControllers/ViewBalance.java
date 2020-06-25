53cfd74bacdd6905878e1c4c176b40340a5601a7
Amin <hasanzamin80@gmail.com>
2020-06-26 02:06:38 +0430

    add view balance page

--- src/main/java/ViewControllers/ViewBalance.java
+++ src/main/java/ViewControllers/ViewBalance.java
@@ -1,4 +1,23 @@
 package ViewControllers;
 
-public class ViewBalance {
+import Controller.CustomerBoss;
+import Model.Account;
+import Model.Customer;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewBalance implements Initializable {
+    public Label balance;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateScreen();
+    }
+
+    public void updateScreen() {
+        balance.setText(Double.toString(CustomerBoss.showMoney((Customer) Account.getOnlineAccount())));
+    }
 }
