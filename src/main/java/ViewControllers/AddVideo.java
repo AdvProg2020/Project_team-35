2041e321c27d3580d33c956fd18b6ed3884d7afd
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2020-06-26 21:14:30 +0430

    add browser for AddVideo.fxml

--- src/main/java/ViewControllers/AddVideo.java
+++ src/main/java/ViewControllers/AddVideo.java
@@ -3,27 +3,36 @@ package ViewControllers;
 import Main.Main;
 import Model.Product;
 import MusicPlayer.MusicPlayer;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Paint;
+import javafx.stage.FileChooser;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddVideo {
+public class AddVideo implements Initializable {
     public TextField url;
     public MediaView media;
     public Label problem;
+    public AnchorPane pane;
 
     public void confirm(MouseEvent mouseEvent) throws IOException {
-
+        MusicPlayer.getInstance().playButtonMusic();
         if (media.getMediaPlayer()==null){
             problem.setText("upload successfully \n click on video to play");
             problem.setTextFill(Paint.valueOf("green"));
-            Media video = new Media(url.getText());
+            String address = "file:///"+url.getText();
+            address = address.replace("\\","//");
+            Media video = new Media(address);
             MediaPlayer mediaPlayer = new MediaPlayer(video);
             media.setMediaPlayer(mediaPlayer);
             Product.getWhoWantsPic().setMedia(video);
@@ -44,5 +53,21 @@ public class AddVideo {
             media.getMediaPlayer().stop();
             media.getMediaPlayer().setAutoPlay(false);
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Button browse = new Button("Browse");
+        FileChooser fc = new FileChooser();
+        FileChooser.ExtensionFilter png = new FileChooser.ExtensionFilter("MP4", "*.MP4");
+        FileChooser.ExtensionFilter jpg = new FileChooser.ExtensionFilter("MKV", "*.MKV");
+        fc.getExtensionFilters().addAll(png,jpg);
+        browse.setOnAction(e->{
+            MusicPlayer.getInstance().playButtonMusic();
+            url.setText(fc.showOpenDialog(pane.getScene().getWindow()).getAbsolutePath());
+        });
+        pane.getChildren().add(browse);
+        browse.setTranslateY(135);
+        browse.setTranslateX(500);
     }
 }
